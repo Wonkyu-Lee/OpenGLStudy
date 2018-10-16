4015fd3223f0779fa8c5c6a8f16c24ff8ae9ab2f
Wonkyu-Lee <wkyulees@gmail.com>
2018-10-17 00:21:10 +0900

    Updated.

--- TessellationApp.cpp
+++ TessellationApp.cpp
@@ -23,6 +23,31 @@ void main(void) {
 }
 )";
 
+const GLchar* TESS_CONTROL_SHADER = R"(
+#version 410 core
+layout (vertices = 3) out;
+void main(void) {
+    if (gl_InvocationID == 0) {
+        gl_TessLevelInner[0] = 5.0;
+        gl_TessLevelOuter[0] = 5.0;
+        gl_TessLevelOuter[1] = 5.0;
+        gl_TessLevelOuter[2] = 5.0;
+    }
+    gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
+}
+)";
+
+const GLchar* TESS_EVALUATION_SHADER = R"(
+#version 410 core
+layout (triangles, equal_spacing, cw) in;
+void main(void) {
+    gl_Position =
+        (gl_TessCoord.x * gl_in[0].gl_Position) +
+        (gl_TessCoord.y * gl_in[1].gl_Position) +
+        (gl_TessCoord.z * gl_in[2].gl_Position);
+}
+)";
+
 const GLchar* FRAGMENT_SHADER = R"(
 #version 410 core
 out vec4 color;
@@ -39,21 +64,29 @@ Application* Application::create() {
 
 void TessellationApp::onAcquireContext() {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // glPatchParameteri(GL_PATCH_VERTICES, 3);
 
     GLuint vertexShader = gl::compileShader(VERTEX_SHADER, GL_VERTEX_SHADER);
+    GLuint tessControlShader = gl::compileShader(TESS_CONTROL_SHADER, GL_TESS_CONTROL_SHADER);
+    GLuint tessEvaluationShader = gl::compileShader(TESS_EVALUATION_SHADER, GL_TESS_EVALUATION_SHADER);
     GLuint fragmentShader = gl::compileShader(FRAGMENT_SHADER, GL_FRAGMENT_SHADER);
 
     _program = glCreateProgram();
     glAttachShader(_program, vertexShader);
-
+    glAttachShader(_program, tessControlShader);
+    glAttachShader(_program, tessEvaluationShader);
     glAttachShader(_program, fragmentShader);
     glLinkProgram(_program);
+    gl::flushErrors(cerr);
 
     glDeleteShader(vertexShader);
+    glDeleteShader(tessControlShader);
+    glDeleteShader(tessEvaluationShader);
     glDeleteShader(fragmentShader);
 
     glGenVertexArrays(1, &_vao);
-
     gl::flushErrors(cerr);
 }
 
@@ -100,5 +133,6 @@ void TessellationApp::onDraw() {
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(_program);
     glBindVertexArray(_vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_PATCHES, 0, 3);
+    gl::flushErrors(cerr);
 }
